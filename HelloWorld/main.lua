389e10989f234d020cd4ff331f98522b7e811046
PHI <kuromilkh@gmail.com>
2015-10-05 15:20:26 +0800

    update

--- HelloWorld/main.lua
+++ HelloWorld/main.lua
@@ -14,13 +14,13 @@ jumpflag = true;
 --physics
 local physics = require("physics");
 physics.start();
-physics.setGravity(0,9.8);
+physics.setGravity(0,6.8);
 --bg settings
 local background = display.newImage("jpg/background2.png");
 background.x = 0;
 background.y = display.contentHeight/2;
 local background2 = display.newImage("jpg/background2.png");
-background2.x = background.x+3599;
+background2.x = background.x+3598;
 background2.y = display.contentHeight/2;
 --car settings
 local car = display.newImage("jpg/car2.png");
@@ -33,14 +33,14 @@ physics.addBody(car,"dynamic",{friction=0, bounce = 0.1});
 --ground
 local ground = display.newRect(car.x, car.y+20,100,2)
 ground.name = "ground";
-ground:setFillColor(1);
+ground:setFillColor(0,0,0,0);
 physics.addBody(ground,"static",{friction=0});
 --traps
 local traps = {};
 --crater
 for i=1,craternum do
 	local crater = display.newImage("jpg/crater0.png");
-	crater.x = -1*(background.x + 100 + i*math.random(450,1250));
+	crater.x = background.x + 100 + i*math.random(450,1250);
 	crater.y = background.y + 41;
 	crater.name = "obstacle";
 	physics.addBody(crater,"static",{friction=0});
@@ -48,7 +48,7 @@ for i=1,craternum do
 end
 --rocks
 for i=craternum,craternum+rocknum do
-	local rock = display.newImage("jpg/rock0.png");
+	rock = display.newImage("jpg/rock0.png");
 	rock.x = background.x + 100 + 2*(i-1)*math.random(150,250);
 	rock.y = background.y + 20;
 	rock.name = "obstacle";
@@ -83,16 +83,19 @@ end
 local function onCollision(self, event)
 	--kill car
 	print(self.name);
-	print(self.target.name);
 	if (self.name == "car" and event.other.name == "obstacle") then
 		carDied = true;
- 		local tm = timer.performWithDelay(200, removeObj, 1)
+ 		local tm = timer.performWithDelay(1, removeObj, 1)
  		tm.params = {param1 = self}
  	end
  	--local params = self.source.params
  	--give car jumpflag
  	if (self.name == "flybullet" and event.other.name == "obstacle") then
- 		print("hitttttttt")
+ 		print("hitrock")
+ 		local tm1 = timer.performWithDelay(1, removeObj, 1)
+ 		tm1.params = {param1 = self}
+ 		local tm2 = timer.performWithDelay(1, removeObj, 1)
+ 		tm2.params = {param1 = event.other}
  		--jumpflag = true;
  		--print("ground give jump")
  	end
@@ -100,10 +103,13 @@ local function onCollision(self, event)
 
 function fireshooot(event)
 	if (car ~= nil) and (car.x ~= nil) then
-		local bullet = display.newRect(car.x+150, car.y,10,30);
+		bullet = display.newRect(car.x+60, car.y,10,3);
+		--bullet = display.newImage("jpg/car2.png");
+		--bullet.x = car.x + 150;
+		--bullet.y = car.y;
 		bullet.name = "flybullet";
+		physics.addBody(bullet,"dynamic",{density=1,friction=0});
 		bullet.gravityScale = 0;
-		physics.addBody(bullet,"static",{friction=0});
 		bullet.collision = onCollision;
 		bullet:addEventListener("collision",bullet);
 		local tm = timer.performWithDelay(1, flyingshoot, -1);
