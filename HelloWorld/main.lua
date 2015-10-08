18a06f60a42774c2cc146541fb224e4e32249174
PHI <kuromilkh@gmail.com>
2015-10-08 21:08:41 +0800

    fixed enroll

--- HelloWorld/main.lua
+++ HelloWorld/main.lua
@@ -14,17 +14,18 @@ jumpflag = true;
 --physics
 local physics = require("physics");
 physics.start();
-physics.setGravity(0,6.8);
+physics.setGravity(0,9.8);
 --bg settings
 local background = display.newImage("jpg/background2.png");
-background.x = 0;
+background.x = 1756;
+print(display.contentWidth)
 background.y = display.contentHeight/2;
 local background2 = display.newImage("jpg/background2.png");
-background2.x = background.x+3598;
+background2.x = background.x+35980;
 background2.y = display.contentHeight/2;
 --car settings
 local car = display.newImage("jpg/car2.png");
-car.x = background.x + 40;
+car.x = 40;
 car.y = background.y + 20;
 car.name = "car";
 opos = car.y;
@@ -36,9 +37,6 @@ ground.name = "ground";
 ground:setFillColor(1);
 physics.addBody(ground,"static",{friction=0});
 
-local win = display.newImage("jpg/win2.png");
-win.x = background.x + 3590;
-win.y = background.y+55;
 
 --traps
 local traps = {};
@@ -46,7 +44,7 @@ local traps = {};
 --for i=1,craternum do
 	i=1;
 	local crater = display.newImage("jpg/crater0.png");
-	crater.x = background.x + 500;
+	crater.x = 500;
 	crater.y = background.y + 42;
 	crater.name = "obstacle";
 	physics.addBody(crater,"static",{friction=0});
@@ -54,7 +52,7 @@ local traps = {};
 	i = i+1;
 
 	local rock = display.newImage("jpg/rock0.png");
-	rock.x = background.x + 350;
+	rock.x = 350;
 	rock.y = background.y + 20;
 	rock.name = "obstacle";
 	physics.addBody(rock,"static",{friction=0});
@@ -62,7 +60,7 @@ local traps = {};
 	i=i+1;
 
 	local crater = display.newImage("jpg/crater0.png");
-	crater.x = background.x + 1500;
+	crater.x = 1500;
 	crater.y = background.y + 42;
 	crater.name = "obstacle";
 	physics.addBody(crater,"static",{friction=0});
@@ -73,7 +71,7 @@ local traps = {};
 --rocks
 for a=c,c+3 do
 	local rock = display.newImage("jpg/rock0.png");
-	rock.x = background.x + 2500 + (c-a)*50;
+	rock.x = 2500 + (c-a)*50;
 	rock.y = background.y + 20;
 	rock.name = "obstacle";
 	physics.addBody(rock,"static",{friction=0});
@@ -82,7 +80,7 @@ for a=c,c+3 do
 end
 
 local function jump(obj)
-	obj:applyForce(0,-6,obj.x,obj.y+5)
+	obj:applyForce(0,-6,obj.x,obj.y)
 	print("jump!!!!!")
 	jumpflag = false;
 end
@@ -112,6 +110,9 @@ local function onCollision(self, event)
 		carDied = true;
  		local tm = timer.performWithDelay(1, removeObj, 1)
  		tm.params = {param1 = self}
+ 		--gameover
+ 		--
+ 		--
  	end
  	--bullet hit obstacles
  	if (self.name == "flybullet" and event.other.name == "obstacle") then
@@ -124,13 +125,13 @@ local function onCollision(self, event)
  	if (self.name == "car" and event.other.name == "ground") then
  		if(jumpflag==false) then
  		jumpflag = true;
- 		print("ground,give jumpable");
+ 		--print("ground,give jumpable");
  	end
  	end
  end
 
 function fireshooot(event)
-	print("fire")
+	--print("fire")
 	if (car ~= nil) and (car.x ~= nil) then
 		bullet = display.newRect(car.x+60, car.y,10,3);
 		bullet.name = "flybullet";
@@ -147,18 +148,18 @@ end
 
 local function handleSwipe(event)
 	--print("swipe");
-	print(jumpflag);
+	--print(jumpflag);
 	if (event.phase == "moved" and jumpflag==true) then
 
 		dy = event.y - event.yStart;
 		if(dy < -1) then
 			jumpflag = false;
 			jump(event.target);
-			print("jumped");
+			--print("jumped");
 		end
 	elseif (event.phase == "ended" or event.phase == "cancelled") then
 		jumpflag = false
-		print("finishjump")
+		--print("finishjump")
 	end
 	return true
 end
@@ -166,7 +167,7 @@ end
 local drawTraps = function( event)
 	for i,obj in ipairs(traps) do
 		if obj.x ~= nil then
-			obj.x = obj.x -4
+			obj.x = obj.x - 5
 		end
 	end
 end
@@ -178,13 +179,14 @@ function bgupdate( event )
 
 	-- body
 end
-local square = display.newRect(0,0,100,100)
-local w,h = display.contentWidth, display.contentHeight;
+
 local listener1 = function( obj )
+	--win
+	--
 	print("Transition 1 completed on object: " .. tostring(obj))
 end
-transition.to(square,{time=15000,alpha=0,x=w/2,y=h/4, onComplete=listener1});
-Runtime:addEventListener("enterFrame", bgupdate);
+transition.to(background,{time=30000,alpha=1,x=-1800,y=background.y, onComplete=listener1});
+--Runtime:addEventListener("enterFrame", bgupdate);
 Runtime:addEventListener("enterFrame", drawTraps);
 Runtime:addEventListener("tap", fireshooot);
 car:addEventListener("touch",handleSwipe);
